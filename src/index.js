7d2b9e03e96ec330737431b84a07b2cc69f3611c
JoonsubHwang <j97194719@gmail.com>
2021-08-31 11:49:47 -0400

    Added BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './index.sass';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter> 
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
