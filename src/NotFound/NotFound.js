d7728e902394e1da5271145ea194a3b2a231cb57
JoonsubHwang <j97194719@gmail.com>
2021-08-31 12:12:32 -0400

    Created NotFound

---
+++ src/NotFound/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './NotFound.sass';
+
+export default class NotFound extends React.Component {
+
+    render = () =>
+        <div>
+            NOTFOUND
+        </div>
+}
