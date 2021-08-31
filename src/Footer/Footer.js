5c65727f68e2efe56b7155f8d63f6208435bf1eb
JoonsubHwang <j97194719@gmail.com>
2021-08-31 12:06:44 -0400

    Created Footer

---
+++ src/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.sass';
+
+export default class Footer extends React.Component {
+
+    render = () =>
+        <div>
+            FOOTER
+        </div>
+}
