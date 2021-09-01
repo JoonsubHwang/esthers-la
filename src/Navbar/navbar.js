d299b27ca5100255037cfeb52dbbfc9eac16fc7b
JoonsubHwang <j97194719@gmail.com>
2021-09-01 17:05:48 -0400

    Focus on searchbar on mount

--- src/Navbar/navbar.js
+++ src/Navbar/navbar.js
@@ -5,22 +5,30 @@ import './Navbar.sass';
 
 export default class Navbar extends React.Component {
 
+    componentDidMount = () => {
+        document.querySelector('#navbar-search>input').focus();
+    }
+
     render = () =>
         <div id='navbar-main'>
             <div id='navbar-container'>
                 <div id='navbar-content'>
+
                     <Link id='navbar-logo' to={'/'}>
                         <p id='navbar-logo-symbol'>LOG</p>
                         <p id='navbar-logo-name'>Esthers.LA</p>
                     </Link>
+
                     <form id='navbar-search'>    
                         <input type='text' placeholder='Character Search'></input>
                         <button><Search aria-label='search' /></button>
                     </form>
+
                     <ul id='navbar-links'>
                         <li><Link to={'/library'}>Library</Link></li>
                         <li><Link to={'/tools'}>Tools</Link></li>
                     </ul>
+                    
                 </div>
             </div>
         </div>
