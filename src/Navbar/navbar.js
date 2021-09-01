5287191a6e7a6c67b791fd104b90447186f8ccaa
JoonsubHwang <j97194719@gmail.com>
2021-09-01 16:03:18 -0400

    Created Navbar layout

--- src/Navbar/navbar.js
+++ src/Navbar/navbar.js
@@ -4,7 +4,23 @@ import './Navbar.sass';
 export default class Navbar extends React.Component {
 
     render = () =>
-        <div>
-            NAVBAR
+        <div id='navbar-main'>
+            <div id='navbar-container'>
+                <div id='navbar-content'>
+                    <div id='navbar-logo'>
+                        <p id='navbar-logo-symbol'>LOG</p>
+                        <p id='navbar-logo-name'>Esthers.LA</p>
+                    </div>
+                    <div id='navbar-search'>    
+                        <p>Q</p>
+                        <input type='text' placeholder='Character Search'></input>
+                    </div>
+                    <ul id='navbar-links'>
+                        <li><a>Library</a></li>
+                        <li><a>Tools</a></li>
+                    </ul>
+                    <p id='navbar-icon'>M</p>
+                </div>
+            </div>
         </div>
 }
