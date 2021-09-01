98d29aef85741747c85093450f1b62c86b2acff2
JoonsubHwang <j97194719@gmail.com>
2021-09-01 11:37:43 -0400

    Removed index.sass import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
-import './index.sass';
 import App from './App/App';
 
 ReactDOM.render(
