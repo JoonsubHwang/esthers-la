6eed875fcec96c37810d584e92a752376c736f87
JoonsubHwang <j97194719@gmail.com>
2021-09-01 18:00:21 -0400

    Added a test: 'can navigate through Navbar links'

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,8 +1,21 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 describe('App', () => {
+
     test('renders App componenet', () => {
         render(<App />);
     });
+
+    test('can navigate through Navbar links', () => {
+
+        render(<App />);
+
+        userEvent.click(screen.getByText('Library'));
+
+        expect(screen.getByText('NOTFOUND')).toBeInTheDocument();
+
+    });
+
 });
