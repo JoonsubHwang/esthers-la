da4a44be0606a79dfd3ca2a81cf271c9b3f16207
JoonsubHwang <j97194719@gmail.com>
2021-09-08 12:41:43 -0400

    Added #main-searchbar

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Search } from '@material-ui/icons';
 import './Main.sass';
 
 export default class Main extends React.Component {
 
     render = () =>
         <main id='main'>
+
             <Link id='main-logo' to={'/'}>
                 <p id='main-logo-symbol'>Es</p>
                 <p id='main-logo-name'>Esthers.LA</p>
             </Link>
+
+            <form id='main-searchbar'>    
+                <input type='text' placeholder='Search character'></input>
+                <button><Search aria-label='search' /></button>
+            </form>
+
         </main>
 }
