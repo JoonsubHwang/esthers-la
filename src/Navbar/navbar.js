e404b31741c96bbd21b2dcfc78b152783328bfa9
JoonsubHwang <j97194719@gmail.com>
2021-09-08 17:08:35 -0400

    Removed #navbar-container

--- src/Navbar/navbar.js
+++ src/Navbar/navbar.js
@@ -9,26 +9,24 @@ export default class Navbar extends React.Component {
 
     render = () =>
         <div id='navbar'>
-            <div id='navbar-container'>
-                <div id='navbar-content'>
+            <div id='navbar-content'>
 
-                    <Link id='navbar-logo' to={'/'}>
-                        <p id='navbar-logo-symbol'>Es</p>
-                        <p id='navbar-logo-name'>Esthers.LA</p>
-                    </Link>
+                <Link id='navbar-logo' to={'/'}>
+                    <p id='navbar-logo-symbol'>Es</p>
+                    <p id='navbar-logo-name'>Esthers.LA</p>
+                </Link>
 
-                    <form id='navbar-searchbar'>    
-                        <input type='text' placeholder='Search character'></input>
-                        <button><Search aria-label='search' /></button>
-                    </form>
+                <form id='navbar-searchbar'>    
+                    <input type='text' placeholder='Search character'></input>
+                    <button><Search aria-label='search' /></button>
+                </form>
 
-                    <ul id='navbar-links'>
-                        <li><Link to={'/news'}><p>News</p></Link></li>
-                        <li><Link to={'/tools'}><p>Tools</p></Link></li>
-                        <li><Link to={'/library'}><p>Library</p></Link></li>
-                    </ul>
+                <ul id='navbar-links'>
+                    <li><Link to={'/news'}><p>News</p></Link></li>
+                    <li><Link to={'/tools'}><p>Tools</p></Link></li>
+                    <li><Link to={'/library'}><p>Library</p></Link></li>
+                </ul>
 
-                </div>
             </div>
         </div>
 }
