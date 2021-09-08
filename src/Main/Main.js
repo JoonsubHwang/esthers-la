d76e96e358f8ee33e06f5012580bd0330f35d741
JoonsubHwang <j97194719@gmail.com>
2021-09-08 11:41:00 -0400

    Created #main and #main-logo

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Main.sass';
 
 export default class Main extends React.Component {
 
     render = () =>
-        <div>
-            MAIN
-        </div>
+        <main id='main'>
+            <Link id='main-logo' to={'/'}>
+                <p id='main-logo-symbol'>Es</p>
+                <p id='main-logo-name'>Esthers.LA</p>
+            </Link>
+        </main>
 }
