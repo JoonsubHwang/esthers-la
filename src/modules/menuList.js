eb82f9711dfd0a7708e84c0b01e7f44aab74f2e2
JoonsubHwang <j97194719@gmail.com>
2021-09-09 14:11:06 -0400

    Created menuList.js

---
+++ src/modules/menuList.js
@@ -0,0 +1,72 @@
+import {ReactComponent as UpArrow} from '../images/up-arrow.svg';
+import {ReactComponent as Event} from '../images/event.svg';
+import {ReactComponent as Megaphone} from '../images/megaphone.svg';
+import {ReactComponent as Armor} from '../images/armor.svg';
+import {ReactComponent as Seal} from '../images/seal.svg';
+import {ReactComponent as Rune} from '../images/rune.svg';
+import {ReactComponent as Card} from '../images/card.svg';
+import {ReactComponent as Math} from '../images/math.svg';
+import {ReactComponent as AI} from '../images/ai.svg';
+
+export const menuList = [
+    {
+        name: 'News',
+        submenus: [
+            {
+                name: 'Updates',
+                url: '/news/updates',
+                icon: <UpArrow />
+            },
+            {
+                name: 'Events',
+                url: '/news/events',
+                icon: <Event />
+            },
+            {
+                name: 'Notices',
+                url: '/news/notices',
+                icon: <Megaphone />
+            }
+        ]
+    },
+    {
+        name: 'Library',
+        submenus: [
+            {
+                name: 'Equip\'s',
+                url: '/library/equipments',
+                icon: <Armor />
+            },
+            {
+                name: 'Seals',
+                url: '/library/seals',
+                icon: <Seal />
+            },
+            {
+                name: 'Runes',
+                url: '/library/runes',
+                icon: <Rune />
+            },
+            {
+                name: 'Cards',
+                url: '/library/cards',
+                icon: <Card />
+            }
+        ]
+    },
+    {
+        name: 'Tools',
+        submenus: [
+            {
+                name: 'SealCalc',
+                url: '/tools/seal-calc',
+                icon: <Math />
+            },
+            {
+                name: 'CarvAI',
+                url: '/tools/carv-ai',
+                icon: <AI />
+            }
+        ]
+    }
+];
