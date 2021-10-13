d41c560d2ff19b66c3e1d901e1365cceaec9ebc6
JoonsubHwang <j97194719@gmail.com>
2021-10-13 12:47:40 -0400

    Filled NotFOund with contents

--- src/NotFound/NotFound.js
+++ src/NotFound/NotFound.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './NotFound.sass';
 
 export default class NotFound extends React.Component {
 
     render = () =>
-        <div>
-            NOTFOUND
-        </div>
+        <main id='notfound'>
+            <h1>Error</h1>
+            <p>NOT FOUND</p>
+            <Link to='/'><button>Back to Homepage</button></Link>
+        </main>
 }
