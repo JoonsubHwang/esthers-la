2120973ed22c4f9b8474ab8ac602ab7ab8f43ad5
JoonsubHwang <j97194719@gmail.com>
2021-10-13 15:06:43 -0400

    Changed 'NOTFOUND' to 'NOT FOUND'

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -14,7 +14,7 @@ describe('App', () => {
 
         userEvent.click(screen.getByText('Library'));
 
-        expect(screen.getByText('NOTFOUND')).toBeInTheDocument();
+        expect(screen.getByText('NOT FOUND')).toBeInTheDocument();
 
     });
 
