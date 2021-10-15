5ef7ca40cbe4190697a1d7840e13f7190917e3aa
JoonsubHwang <j97194719@gmail.com>
2021-10-15 12:36:15 -0400

    Added 'search character using searchbars' test

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -6,15 +6,34 @@ describe('App', () => {
 
     test('renders App componenet', () => {
         render(<App />);
+        expect(screen.getAllByRole('link')).toHaveLength(1 + 3 + 1 + 9);
     });
 
-    test('can navigate through Navbar links', () => {
+    test('navigate through Navbar links', () => {
 
         render(<App />);
-
+        
         userEvent.click(screen.getByText('Library'));
 
         expect(screen.getByText('NOT FOUND')).toBeInTheDocument();
+
+    });
+
+    test('search character using searchbars', () => {
+
+        render(<App />);
+        
+        // header searchbar
+        userEvent.type(screen.getAllByRole('textbox')[0], 'CharacterName');
+        userEvent.click(screen.getAllByRole('button')[0]);
+        expect(screen.getByText('CharacterName')).toBeInTheDocument();
+
+        userEvent.click(screen.getByText('Esthers.LA'));
+        
+        // homepage searchbar
+        userEvent.type(screen.getAllByRole('textbox')[1], 'CharacterName');
+        userEvent.click(screen.getAllByRole('button')[1]);
+        expect(screen.getByText('CharacterName')).toBeInTheDocument();
 
     });
 
