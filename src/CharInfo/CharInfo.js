cb38393b48cdcce73928e19b0a48b44a72c970f6
JoonsubHwang <j97194719@gmail.com>
2021-10-15 17:59:26 -0400

    Changed tabs to toggle

--- src/CharInfo/CharInfo.js
+++ src/CharInfo/CharInfo.js
@@ -4,6 +4,31 @@ import './CharInfo.sass';
 
 
 export default class CharInfo extends React.Component {
+    
+    state = {
+        category: true
+    }
+
+    toggleCategory = () => {
+
+        this.setState({ category: !this.state.category });
+
+        console.debug(this.state.category)
+
+        if (this.state.category) {
+            document.querySelector('#categories').classList.remove('reset-position');
+            document.querySelector('#subcategories').classList.remove('reset-position');
+            document.querySelector('#categories').classList.add('move-up');
+            document.querySelector('#subcategories').classList.add('move-up');
+        }
+        else {
+            document.querySelector('#categories').classList.remove('move-up');
+            document.querySelector('#subcategories').classList.remove('move-up');
+            document.querySelector('#categories').classList.add('reset-position');
+            document.querySelector('#subcategories').classList.add('reset-position');
+        }
+
+    }
 
     render = () =>
         <main id='charinfo'>
@@ -15,22 +40,55 @@ export default class CharInfo extends React.Component {
                 <p>In Servername</p>
             </div>
 
-            <div id='details'>
-                <ul id='categories'>
-                    <li className='selected'>TAB</li>
-                    <li>TAB</li>
-                    <li>TAB</li>
-                    <li>TAB</li>
-                </ul>
-                <ul id='subcategories'>
-                    <li className='selected'>Equip</li>
-                    <li>Avatar</li>
-                    <li>Trait</li>
-                    <li>Jewel</li>
-                    <li>Kard</li>
-                </ul>
-                <div>
-                    CONTENTS
+            <div id='info'>
+                <div id='tabs' onClick={this.toggleCategory}>
+                    <ul id='categories'>
+                        <li className='selected'>TAB</li>
+                        <li>TAB</li>
+                        <li>TAB</li>
+                        <li>TAB</li>
+                    </ul>
+                    <ul id='subcategories'>
+                        <li className='selected'>Equip</li>
+                        <li>Avatar</li>
+                        <li>Trait</li>
+                        <li>Jewel</li>
+                        <li>Kard</li>
+                    </ul>
+                </div>
+                <div id='details'>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
+                    <p>CONTENTS</p>
                 </div>
             </div>
         </main>
