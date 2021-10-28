f34d827782f632ad449a4375779cc642b9d9bf36
JoonsubHwang <j97194719@gmail.com>
2021-10-28 16:42:03 -0400

    Moved backend/index.js into App.js

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics'; // more: https://firebase.google.com/docs/web/setup#available-libraries
 import './App.sass';
 import Header from '../Header/Header';
 import Homepage from '../Homepage/Homepage';
@@ -8,6 +10,23 @@ import NotFound from '../NotFound/NotFound';
 import Footer from '../Footer/Footer';
 import Navbar from '../Navbar/Navbar';
 import minWidth from '../modules/minWidth';
+
+
+
+// backend initialization 
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBSHmyZ2Fmt_5ZfPY9wOa3UrG7eA56WRpI',
+  authDomain: 'esthers-la.firebaseapp.com',
+  projectId: 'esthers-la',
+  storageBucket: 'esthers-la.appspot.com',
+  messagingSenderId: '926035998720',
+  appId: '1:926035998720:web:f43e580cdad50252ae80e5',
+  measurementId: 'G-G7QE7JQLYX'
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 
 
