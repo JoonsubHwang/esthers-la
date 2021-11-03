f0a982a4b96d0616c28d7421d61923c353d8415c
JoonsubHwang <j97194719@gmail.com>
2021-11-03 15:50:57 -0400

    Added charData function group

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin')
+const admin = require('firebase-admin');
 
 admin.initializeApp();
+
+exports.charData = require('./charData');
